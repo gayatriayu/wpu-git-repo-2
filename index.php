b7881e62fca7463ee4c122afe7cbedef2881ac7e
gytrvi <gayatrimade30@gmail.com>
2021-03-15 12:54:52 +0700

    menambahkan paragraph

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Git Remote</title>
+</head>
+<body>
+    <h1>Helloo World</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur enim, exercitationem odit dolorum necessitatibus quo facere quae perferendis cupiditate autem expedita, ad minima, doloribus quas! Excepturi non repellendus et necessitatibus?</p>
+
+    
+</body>
+</html>
